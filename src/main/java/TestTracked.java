e55cba53291cedf5f2ef9ab7d9ab03c2db9ae0e4
Munarbek Niyazbekov <pro100chok2003@gmail.com>
2022-06-26 16:13:30 +0600

    Added Test1

---
+++ src/main/java/TestTracked.java
@@ -0,0 +1,5 @@
+public class TestTracked {
+    public static void main(String[] args) {
+        System.out.println("This is test class");
+    }
+}
---
+++ src/main/java/TestTracked1.java
@@ -0,0 +1,5 @@
+public class TestTracked1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
